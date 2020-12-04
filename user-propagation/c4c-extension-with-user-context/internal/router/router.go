0a54c04e6902f9a5ca64989b73a574f57bdef741
Jamie Cawley <jamie.cawley@sap.com>
2020-12-04 17:19:21 -0500

    switching api url

--- user-propagation/c4c-extension-with-user-context/internal/router/router.go
+++ user-propagation/c4c-extension-with-user-context/internal/router/router.go
@@ -1,13 +1,14 @@
 package router
 
 import (
-	"github.com/gorilla/mux"
-	"github.com/SAP-samples/kyma-runtime-extension-samples/user-propagation/c4c-extension-with-user-context/internal/handlers"
 	"net/http"
+
+	"github.com/SAP-samples/kyma-runtime-extension-samples/user-propagation/c4c-extension-with-user-context/internal/handlers"
+	"github.com/gorilla/mux"
 )
 
 func New() http.Handler {
 	router := mux.NewRouter()
-	router.HandleFunc("/tasks", handlers.NewDispatcher().CreateTask).Methods(http.MethodPost)
+	router.HandleFunc("/api/tasks", handlers.NewDispatcher().CreateTask).Methods(http.MethodPost)
 	return router
 }
