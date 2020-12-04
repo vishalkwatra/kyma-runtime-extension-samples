c25260eaaea99b02b965cf078e2a78c1e85ef025
Jamie Cawley <jamie.cawley@sap.com>
2020-12-04 15:09:58 -0500

    adding wip example

--- user-propagation/angular-example-app/src/app/app.component.ts
+++ user-propagation/angular-example-app/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { OidcConfigService, OidcSecurityService, ConfigResult, AuthorizationState, AuthorizationResult } from 'angular-auth-oidc-client';
 import { Router } from '@angular/router';
 
 @Component({
@@ -15,86 +14,15 @@ export class AppComponent implements OnInit, OnDestroy {
 
   constructor(
     private router: Router,
-    private oidcConfigService: OidcConfigService,
-    public oidcSecurityService: OidcSecurityService
   ) {
-    if (this.oidcSecurityService.moduleSetup) {
-      this.doCallbackLogicIfRequired();
-    } else {
-      this.oidcSecurityService.onModuleSetup.subscribe(() => {
-        this.doCallbackLogicIfRequired();
-      });
-    }
-
-    this.oidcSecurityService.onAuthorizationResult.subscribe(
-      (authorizationResult: AuthorizationResult) => {
-        this.onAuthorizationResultComplete(authorizationResult);
-      });
   }
 
   ngOnInit() {
-    this.oidcConfigService.onConfigurationLoaded.subscribe((value: ConfigResult) => {
-      this.isConfigurationLoaded = true;
-    });
-
-    this.oidcSecurityService.getIsAuthorized().subscribe(auth => {
-      this.isAuthenticated = auth;
-    });
-
-    this.oidcSecurityService.getUserData().subscribe(userData => {
-      this.userData = userData;
-    });
   }
 
   ngOnDestroy(): void { }
 
-  login() {
-    this.oidcSecurityService.authorize();
-  }
 
-  logout() {
-    this.oidcSecurityService.logoff();
-  }
-
-  private doCallbackLogicIfRequired() {
-    console.log('doCallbackLogicIfRequired');
-    if (window.location.hash) {
-      this.oidcSecurityService.authorizedImplicitFlowCallback();
-    } else {
-      if ('/autologin' !== window.location.pathname) {
-        console.log('autologin');
-        // this.write('redirect', window.location.pathname);
-        // this.write('search', window.location.search);
-        this.write('href', window.location.href);
-      }
-      console.log('AppComponent:onModuleSetup');
-      this.oidcSecurityService.getIsAuthorized().subscribe((authorized: boolean) => {
-        console.log('getIsAuthorized');
-        if (!authorized) {
-          console.log('Is not authorized');
-          this.router.navigate(['/autologin']);
-        }
-      });
-    }
-  }
-
-  private onAuthorizationResultComplete(authorizationResult: AuthorizationResult) {
-    console.log('onAuthorizationResultComplete');
-    // const path = this.read('redirect');
-    // const search = this.read('search');
-    const href = this.read('href');
-
-    console.log('Auth result received AuthorizationState:'
-      + authorizationResult.authorizationState
-      + ' validationResult:' + authorizationResult.validationResult);
-
-    if (authorizationResult.authorizationState === AuthorizationState.authorized) {
-      // this.router.navigate([path], {queryParams: search});
-      window.location.href = href;
-    } else {
-      this.router.navigate(['/Unauthorized']);
-    }
-  }
 
   private read(key: string): any {
     const data = localStorage.getItem(key);
--- user-propagation/angular-example-app/src/app/http-interceptors/index.ts
+++
@@ -1,8 +0,0 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-
-import { OIDCInterceptor } from './oidc-interceptor';
-
-/** Http interceptor providers in outside-in order */
-export const httpInterceptorProviders = [
-  { provide: HTTP_INTERCEPTORS, useClass: OIDCInterceptor, multi: true },
-];
